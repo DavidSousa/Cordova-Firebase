ad1842b659ef4ac060a3b0e0731e9fae10f5622e
Truewind <labs@truewind.pt>
2018-09-20 14:03:07 +0100

    comment out updateStringsXml method invocation

--- scripts/unzipAndCopyConfigurations.js
+++ scripts/unzipAndCopyConfigurations.js
@@ -143,7 +143,7 @@ module.exports = function (context) {
 
   if (platform === constants.android.platform) {
     var contents = fs.readFileSync(sourceFilePath).toString();
-    updateStringsXml(contents, appId);
+    //updateStringsXml(contents, appId);
   }
 
   return defer.promise;
